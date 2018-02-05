62493d66454b76913293cc240a905df0607a1f5a
sbalci <drserdarbalci@gmail.com>
2018-02-05 20:45:10 +0300

    Updated Picture

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Generating Reproducible Report to Compare Different Countries for the Characteristics of Peer Reviewed Articles in Pathology Journals
 
+
 It is a very common practice to retrospectively analyse the number of peer reviewed articles written from a country to view the amount of contribution made to a specific scientific discipline.
+
+
 These studies require too much effort, since the data is generally behind paywalls and restrictions.
+
+
 I have previously contributed to a research to identify the Articles from Turkey Published in Pathology Journals Indexed in International Indexes; which is published here: http://www.turkjpath.org/summary_en.php3?id=1423 DOI: 10.5146/tjpath.2010.01006
+
+
 This study required manually investigating many excel files, which was time consuming and updating the data and results also require a similar amount of effort.
-In order to automatize this 
+
+
+In order to automatize this analysis, I have used PubMed data from National Library of Medicine. This collection has the most comprehensive information about peer reviewed articles in medicine. It also has API, and R packages are available for getting and fetching data from the server.  
--- pathologypubmed.R
+++ pathologypubmed.R
@@ -11,7 +11,7 @@ library(purrr)
 # Load Pathology Journal ISSNs, data retrieved from "incites clarivate:
 # Journal Data Filtered By:  Selected JCR Year: 2016
 # Selected Editions: SCIE,SSCI Selected Categories: 'PATHOLOGY'
-# Selected Category Scheme: WoS"
+# Selected Category Scheme: WoS
 ISSNList <- JournalHomeGrid <- read_csv("JournalHomeGrid.csv", 
                                         skip = 1) %>% 
     select(ISSN) %>% 
@@ -38,38 +38,71 @@ JapanArticles <- EUtilsSummary(searchformulaJP, type = 'esearch', db = 'pubmed',
 fetchJapan <- EUtilsGet(JapanArticles)
 
 # Articles per countries per year
-tableTR <- table(YearReceived(fetchTurkey)) %>% 
+tableTR <- table(YearPubmed(fetchTurkey)) %>% 
     as_tibble() %>% 
-    rename(Turkey = n)
+    rename(Turkey = n, Year = Var1)
 
-tableDE <- table(YearReceived(fetchGermany)) %>% 
+tableDE <- table(YearPubmed(fetchGermany)) %>% 
     as_tibble() %>% 
-    rename(Germany = n)
+    rename(Germany = n, Year = Var1)
 
-tableJP <- table(YearReceived(fetchJapan)) %>% 
+tableJP <- table(YearPubmed(fetchJapan)) %>% 
     as_tibble() %>% 
-    rename(Japan = n)
+    rename(Japan = n, Year = Var1)
 
 articles_per_year <- list(
     tableTR,
     tableDE,
     tableJP
     ) %>%
-    reduce(left_join, by = "Var1", .id = "id") %>% 
-    gather(Country, n, 2:4) %>% 
-    rename(Year = Var1)
+    reduce(left_join, by = "Year", .id = "id") %>% 
+    gather(Country, n, 2:4)
+
+articles_per_year$Country <- factor(articles_per_year$Country,
+                                       levels =c("Japan", "Germany", "Turkey"))
 
 ## Graph 1 
 
-ggplot(data = articles_per_year, aes(x = Year, y = n, group = Country, colour = Country, shape = Country)) +
+ggplot(data = articles_per_year, aes(x = Year, y = n, group = Country,
+                                     colour = Country, shape = Country,
+                                     levels = Country
+                                     )) +
     geom_line() +
     geom_point() +
     labs(x = "Year", y = "Number of Articles") +
     ggtitle("Pathology Articles Per Year") +
-    theme(plot.title = element_text(hjust = 0.5))
+    theme(plot.title = element_text(hjust = 0.5), 
+          text = element_text(size = 10))
 
-## 
 
+# Articles per journals per country
+
+JournalsTR <- cbind.data.frame(YearPubmed(fetchTurkey), ISSN(fetchTurkey)) %>% 
+    rename( Year = "YearPubmed(fetchTurkey)", ISSN = "ISSN(fetchTurkey)")
+
+JournalsDE <- cbind.data.frame(YearPubmed(fetchGermany), ISSN(fetchGermany)) %>% 
+    rename( Year = "YearPubmed(fetchGermany)", ISSN = "ISSN(fetchGermany)")
+
+JournalsJP <- cbind.data.frame(YearPubmed(fetchJapan), ISSN(fetchJapan)) %>% 
+    rename( Year = "YearPubmed(fetchJapan)", ISSN = "ISSN(fetchJapan)")
+
+
+# articles_per_journal <- list(
+#     JournalsTR,
+#     JournalsDE,
+#     JournalsJP
+# ) %>%
+#     reduce(left_join, by = "ISSN", .id = "id") %>% 
+#     gather(Country, n, 2:4)
+# 
+# # Graph 2
+# articles_per_journal %>% 
+#     filter(n>10 & n<100) %>% 
+# ggplot(aes(x = ISSN, y = n, group = Country, colour = Country, shape = Country)) +
+#     geom_point() +
+#     labs(x = "ISSN", y = "Number of Articles") +
+#     ggtitle("Pathology Articles Per Journal") +
+#     theme(plot.title = element_text(hjust = 0.5))
 
 
 
