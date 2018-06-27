c7792f2b698ebd6b1e420bf5390b830b85a5b820
sbalci <drserdarbalci@gmail.com>
2018-06-27 10:45:03 +0300

    updated on 2018-06-27 10:35:50

--- PreperationPackagesScheduleRScript.R
+++ PreperationPackagesScheduleRScript.R
@@ -1,21 +1,24 @@
-https://github.com/jkeirstead/scholar
-http://rogiersbart.blogspot.com.tr/2015/05/put-google-scholar-citations-on-your.html
-http://tuxette.nathalievilla.org/?p=1682
-https://github.com/sunitj/scholar
-https://tgmstat.wordpress.com/2013/09/11/schedule-rscript-with-cron/
-
-
-
-install.packages('data.table')
-install.packages('knitr')
-install.packages('miniUI')
-install.packages('shiny')
-install.packages('shinyFiles')
-
+# Preperation Packages Schedule R Script
+# https://github.com/jkeirstead/scholar
+# http://rogiersbart.blogspot.com.tr/2015/05/put-google-scholar-citations-on-your.html
+# http://tuxette.nathalievilla.org/?p=1682
+# https://github.com/sunitj/scholar
+# https://tgmstat.wordpress.com/2013/09/11/schedule-rscript-with-cron/
+# https://github.com/ropensci/git2r
+#     
+# library(knitr)
+# library(markdown)
+# 
+# install.packages('data.table')
+# install.packages('knitr')
+# install.packages('miniUI')
+# install.packages('shiny')
+# install.packages('shinyFiles')
+# 
 # install.packages("taskscheduleR", repos = "http://www.datatailor.be/rcube", type = "source")
-
-devtools::install_github("bnosac/cronR")
-install.packages('scholar')
-
-https://github.com/ropensci/git2r
-
+# 
+# devtools::install_github("bnosac/cronR")
+# install.packages('scholar')
+# 
+# 
+# 
