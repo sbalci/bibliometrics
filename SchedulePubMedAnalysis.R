947c0bcfc33c6946b5cce0d23b207e12a92183b8
sbalci <drserdarbalci@gmail.com>
2018-06-30 10:20:25 +0300

    updated on 2018-06-30 10:20:21

--- SchedulePubMedAnalysis.R
+++ SchedulePubMedAnalysis.R
@@ -1,20 +1,29 @@
 library(rmarkdown)
 library(pander)
+library(rstudioapi)
+
 
 rmarkdown::render(input = "SchedulePubMedAnalysis.Rmd", output_format = "html_notebook", output_file = "docs/SchedulePubMedAnalysis.html"
                    , quiet = TRUE
                   )
 
+Sys.sleep(time = 2)
+
 rmarkdown::render(input = "SchedulePubMedAnalysis2.Rmd", output_format = "html_notebook", output_file = "docs/SchedulePubMedAnalysis2.html"
                    , quiet = TRUE
 )
 
-library(rstudioapi)
+Sys.sleep(time = 2)
 
 CommitMessage <- paste("updated on ", Sys.time(), sep = "")
 gitCommand <- paste("git add . \n git commit --message '", CommitMessage, "' \n git push origin master \n", sep = "")
 
+Sys.sleep(time = 2)
+
 gitTerm <- rstudioapi::terminalCreate(show = FALSE)
+
+Sys.sleep(time = 2)
+
 rstudioapi::terminalSend(gitTerm, gitCommand)
 
 
